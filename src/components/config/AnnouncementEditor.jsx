1cb70bc67639d621a7f0ca22a9988295e9352c2f
colin <42125428+colinhartigan@users.noreply.github.com>
2024-10-09 14:08:41 -0400

    Update AnnouncementEditor.jsx

--- src/components/config/AnnouncementEditor.jsx
+++ src/components/config/AnnouncementEditor.jsx
@@ -37,7 +37,7 @@ export default function AnnouncementEditor({}) {
 
     return (
         <Flex direction="column" gap="2" width="100%" height="100%" p="1">
-            <Flex direction="row" gap="2" width="100%" height="100%" flexGrow="1" justify="center" align="stretch">
+            <Flex direction="row" gap="2" width="100%" height="100%" flexGrow="1" justify="start" align="stretch">
                 <Flex direction="column" className="w-1/3" justify="start" align="center" gap="4">
                     <ScrollArea scrollbars="vertical" type="always" className="h-full max-h-full w-full">
                         <RadioCards.Root
@@ -75,108 +75,110 @@ export default function AnnouncementEditor({}) {
                     </Flex>
                 </Flex>
 
-                <Flex direction="column" className="min-h-full w-2/3" gap="3">
-                    <Flex direction="row" justify="between" align="center" gap="4">
-                        <Flex direction="row" align="center" gap="2">
-                            <Switch
-                                radius="small"
-                                checked={selectedSlide?.enabled}
-                                onCheckedChange={(checked) => {
-                                    setSelectedSlide({
-                                        ...selectedSlide,
-                                        enabled: checked,
-                                    });
-                                    updateSlide(selectedSlideIndex, {
-                                        ...infoSlides[selectedSlideIndex],
-                                        enabled: checked,
-                                        timestamp: dayjs(),
-                                    });
-                                }}
-                            />
-                            <Text size="2">Enable slide</Text>
-                        </Flex>
-                        <AlertDialog.Root>
-                            <AlertDialog.Trigger>
-                                <Button color="red">
-                                    <TrashIcon />
-                                    Delete
-                                </Button>
-                            </AlertDialog.Trigger>
-                            <AlertDialog.Content maxWidth="450px">
-                                <AlertDialog.Title>Delete announcement</AlertDialog.Title>
-                                <AlertDialog.Description size="2">Are you sure?</AlertDialog.Description>
+                {Object.keys(selectedSlide).length > 0 && (
+                    <Flex direction="column" className="min-h-full w-2/3" gap="3">
+                        <Flex direction="row" justify="between" align="center" gap="4">
+                            <Flex direction="row" align="center" gap="2">
+                                <Switch
+                                    radius="small"
+                                    checked={selectedSlide?.enabled}
+                                    onCheckedChange={(checked) => {
+                                        setSelectedSlide({
+                                            ...selectedSlide,
+                                            enabled: checked,
+                                        });
+                                        updateSlide(selectedSlideIndex, {
+                                            ...infoSlides[selectedSlideIndex],
+                                            enabled: checked,
+                                            timestamp: dayjs(),
+                                        });
+                                    }}
+                                />
+                                <Text size="2">Enable slide</Text>
+                            </Flex>
+                            <AlertDialog.Root>
+                                <AlertDialog.Trigger>
+                                    <Button color="red">
+                                        <TrashIcon />
+                                        Delete
+                                    </Button>
+                                </AlertDialog.Trigger>
+                                <AlertDialog.Content maxWidth="450px">
+                                    <AlertDialog.Title>Delete announcement</AlertDialog.Title>
+                                    <AlertDialog.Description size="2">Are you sure?</AlertDialog.Description>
 
-                                <Flex gap="3" mt="4" justify="end">
-                                    <AlertDialog.Cancel>
-                                        <Button variant="soft" color="gray">
-                                            Cancel
-                                        </Button>
-                                    </AlertDialog.Cancel>
-                                    <AlertDialog.Action>
-                                        <Button variant="solid" color="red" onClick={removeSlide}>
-                                            Delete
-                                        </Button>
-                                    </AlertDialog.Action>
-                                </Flex>
-                            </AlertDialog.Content>
-                        </AlertDialog.Root>
-                    </Flex>
-                    <TextField.Root
-                        size="2"
-                        className="min-h-[30px]"
-                        placeholder="Announcement name"
-                        value={selectedSlide?.title}
-                        onChange={(e) =>
-                            setSelectedSlide({
-                                ...selectedSlide,
-                                title: e.target.value,
-                            })
-                        }
-                    />
-                    {showSlidePreview ? (
-                        <Markdown
-                            className="prose prose-lg prose-neutral prose-invert h-auto w-full flex-grow overflow-auto px-2 prose-headings:font-bold"
-                            remarkPlugins={[remarkGfm]}
-                            rehypePlugins={[rehypeRaw]}
-                        >
-                            {selectedSlide?.content}
-                        </Markdown>
-                    ) : (
-                        <Editor
-                            className="w-full flex-grow"
-                            value={selectedSlide?.content}
-                            onChange={(v, e) =>
+                                    <Flex gap="3" mt="4" justify="end">
+                                        <AlertDialog.Cancel>
+                                            <Button variant="soft" color="gray">
+                                                Cancel
+                                            </Button>
+                                        </AlertDialog.Cancel>
+                                        <AlertDialog.Action>
+                                            <Button variant="solid" color="red" onClick={removeSlide}>
+                                                Delete
+                                            </Button>
+                                        </AlertDialog.Action>
+                                    </Flex>
+                                </AlertDialog.Content>
+                            </AlertDialog.Root>
+                        </Flex>
+                        <TextField.Root
+                            size="2"
+                            className="min-h-[30px]"
+                            placeholder="Announcement name"
+                            value={selectedSlide?.title}
+                            onChange={(e) =>
                                 setSelectedSlide({
                                     ...selectedSlide,
-                                    content: v,
+                                    title: e.target.value,
                                 })
                             }
-                            defaultLanguage="html"
-                            theme="vs-dark"
                         />
-                    )}
-                    <Flex gap="3" justify="between">
-                        <Flex direction="row" gap="2" justify="center" align="center">
-                            <Switch
-                                radius="small"
-                                checked={showSlidePreview}
-                                onCheckedChange={(checked) => setShowSlidePreview(checked)}
-                            ></Switch>
-                            <Text size="2">Preview</Text>
+                        {showSlidePreview ? (
+                            <Markdown
+                                className="prose prose-lg prose-neutral prose-invert h-auto w-full flex-grow overflow-auto px-2 prose-headings:font-bold"
+                                remarkPlugins={[remarkGfm]}
+                                rehypePlugins={[rehypeRaw]}
+                            >
+                                {selectedSlide?.content}
+                            </Markdown>
+                        ) : (
+                            <Editor
+                                className="w-full flex-grow"
+                                value={selectedSlide?.content}
+                                onChange={(v, e) =>
+                                    setSelectedSlide({
+                                        ...selectedSlide,
+                                        content: v,
+                                    })
+                                }
+                                defaultLanguage="html"
+                                theme="vs-dark"
+                            />
+                        )}
+                        <Flex gap="3" justify="between">
+                            <Flex direction="row" gap="2" justify="center" align="center">
+                                <Switch
+                                    radius="small"
+                                    checked={showSlidePreview}
+                                    onCheckedChange={(checked) => setShowSlidePreview(checked)}
+                                ></Switch>
+                                <Text size="2">Preview</Text>
+                            </Flex>
+                            <Button
+                                onClick={saveSlide}
+                                disabled={
+                                    // disable if everything between the edited and saved slide is the same except timestamp
+                                    selectedSlide.title === infoSlides[selectedSlideIndex]?.title &&
+                                    selectedSlide.content === infoSlides[selectedSlideIndex]?.content &&
+                                    selectedSlide.enabled === infoSlides[selectedSlideIndex]?.enabled
+                                }
+                            >
+                                Save announcement
+                            </Button>
                         </Flex>
-                        <Button
-                            onClick={saveSlide}
-                            disabled={
-                                // disable if everything between the edited and saved slide is the same except timestamp
-                                selectedSlide.title === infoSlides[selectedSlideIndex]?.title &&
-                                selectedSlide.content === infoSlides[selectedSlideIndex]?.content &&
-                                selectedSlide.enabled === infoSlides[selectedSlideIndex]?.enabled
-                            }
-                        >
-                            Save announcement
-                        </Button>
                     </Flex>
-                </Flex>
+                )}
             </Flex>
         </Flex>
     );
